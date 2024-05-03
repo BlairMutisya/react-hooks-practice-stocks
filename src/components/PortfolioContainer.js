0c83ac935c4fd3d7944ca40147295932e888ba33
BlairMutisya <tonnyblair09@gmail.com>
2024-05-03 03:26:59 +0300

    Complete lab

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import Stock from "./Stock";
 
 function PortfolioContainer() {
+  // State to store the list of stocks in the portfolio
+  const [portfolioStocks, setPortfolioStocks] = useState([
+    // Initial list of stocks in the portfolio
+    { id: 1, name: "Google", ticker: "GOOG", price: 1194.8 },
+    { id: 2, name: "Facebook", ticker: "FB", price: 168.85 },
+    // Add more stocks as needed
+  ]);
+
+  // Function to handle selling a stock
+  const sellStock = (id) => {
+    // Filter out the stock with the given id from the portfolio
+    const updatedPortfolio = portfolioStocks.filter((stock) => stock.id !== id);
+    // Update the portfolio state with the filtered list
+    setPortfolioStocks(updatedPortfolio);
+  };
+
   return (
     <div>
       <h2>My Portfolio</h2>
-      {
-        //render your portfolio stocks here
-      }
+      {/* Render each stock in the portfolio */}
+      {portfolioStocks.map((stock) => (
+        <Stock
+          key={stock.id}
+          name={stock.name}
+          ticker={stock.ticker}
+          price={stock.price}
+          // Attach event handler to sell the stock when clicked
+          onClick={() => sellStock(stock.id)}
+        />
+      ))}
     </div>
   );
 }
--- src/components/StockContainer.js
+++ src/components/StockContainer.js
@@ -2,10 +2,46 @@ import React from "react";
 import Stock from "./Stock";
 
 function StockContainer() {
+  // Sample array of stock data from db.json
+  const stocksData = [
+    {
+      "id": 1,
+      "ticker": "GOOG",
+      "name": "Google",
+      "type": "Tech",
+      "price": 1194.8
+    },
+    {
+      "id": 2,
+      "ticker": "FB",
+      "name": "Facebook",
+      "type": "Tech",
+      "price": 168.85
+    },
+    {
+      "id": 3,
+      "ticker": "TWTR",
+      "name": "Twitter",
+      "type": "Tech",
+      "price": 29.41
+    },
+    
+  ];
+
   return (
     <div>
       <h2>Stocks</h2>
-      {/* render stock list here*/}
+      <div>
+        {stocksData.map((stock) => (
+          <Stock
+            key={stock.id}
+            ticker={stock.ticker}
+            name={stock.name}
+            type={stock.type}
+            price={stock.price}
+          />
+        ))}
+      </div>
     </div>
   );
 }
